ff1ee1286927d1fbb4007ed9bf9cb9f65c1b13a9
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2024-11-18 12:01:24 +0100

    [feat] changed port to support codio

--- main.py
+++ main.py
@@ -302,4 +302,4 @@ with app.app_context() as app_context:
     db.create_all()
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=3000, debug=True)
+    app.run("0.0.0.0", port=5002, debug=True)
